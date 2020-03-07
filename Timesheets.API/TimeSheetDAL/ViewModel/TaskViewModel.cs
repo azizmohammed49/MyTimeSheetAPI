da12285d05ade47addb4fc112d33b9dfb95fa3ff
azizmohammed49 <61844621+azizmohammed49@users.noreply.github.com>
2020-03-07 14:43:35 +0530

    Add files via upload

---
+++ Timesheets.API/TimeSheetDAL/ViewModel/TaskViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TimeSheetDAL.ViewModel
+{
+    public class TaskViewModel
+    {
+        public int EmpID { get; set; }
+        public string Code { get; set; }
+        public string EmpName { get; set; }
+
+        public string TaskName { get; set; }
+        public string Description { get; set; }
+
+       
+    }
+}
